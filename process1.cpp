abd860b0f4d0276d12e9dc8543da0973aef8d629
dagem720 <59290341+dagem720@users.noreply.github.com>
2020-01-01 09:54:51 +0300

    Add files via upload

---
+++ process1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+using namespace std;
+
+int main(){
+int turn,sha_var,sha_vart,x;
+fstream turnRW;
+fstream shared;
+for(int i=1;i<=3;i++)
+{
+turnRW.open("turn.txt",ios::in|ios::out);
+turnRW>>turn;
+//cout<<turn;
+
+turnRW.close();
+
+	
+  while(turn!=1){
+  
+  	turnRW.open("turn.txt",ios::in|ios::out);
+turnRW>>turn;
+ turnRW.close();
+  }
+  cout<<"Process 1 is in critical section now!!"<<endl<<endl;
+   
+ shared.open("shared.txt",ios::in|ios::out|ios::app);
+   ifstream infile; 
+   infile.open("shared.txt"); 
+   infile >> x; 
+   
+   infile.close();
+  
+  
+   ofstream outfile;
+   outfile.open("shared.txt",ios::trunc);
+   cout<<"\tthe Value of shared value received from a file is "<<x<<endl;
+   x=x+1;
+   cout<<"\tthe Value of shared value incremented to "<<x<<endl<<endl;
+   outfile <<x  << endl;
+   outfile.close();
+
+ sleep(1);
+ turnRW.open ("turn.txt", ios::out | ios::in | ofstream::trunc );
+
+	turn=0;
+	cout<<"Process 1 is in non-critical section now!!"<<endl<<endl;
+    turnRW << turn;
+    turnRW.close();
+    sleep(1);
+    
+}
+ 
+ 
+
+}
